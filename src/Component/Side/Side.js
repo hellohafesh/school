9e253fea5b990e54e28a223a4c230468df16cc90
soumik825 <allhafesh825@gmail.com>
2023-01-20 03:50:50 +0600

    add hover effect

--- src/Component/Side/Side.js
+++ src/Component/Side/Side.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import "./Side.css";
 
 const Side = () => {
     return (
@@ -20,19 +21,19 @@ const Side = () => {
             <div className='mb-4'>
                 <h6 className='w-full bg-primary text-white p-3'>গুরুত্বপূর্ণ লিংক</h6>
                 <div className=" row">
-                    <Link className='w-full'>জাতীয় তথ্য বাতায়ন</Link>
-                    <Link className='w-full '>শিক্ষা মন্ত্রণালয়</Link>
-                    <Link className='w-full '>মাধ্যমিক ও উচ্চ শিক্ষা বিভাগ</Link>
-                    <Link className='w-full '>মাধ্যমিক ও উচ্চশিক্ষা অধিদপ্তর</Link>
-                    <Link className='w-full '>দিনাজপুর শিক্ষা বোর্ড</Link>
+                    <Link className='w-full impLink'>জাতীয় তথ্য বাতায়ন</Link>
+                    <Link className='w-full impLink'>শিক্ষা মন্ত্রণালয়</Link>
+                    <Link className='w-full impLink'>মাধ্যমিক ও উচ্চ শিক্ষা বিভাগ</Link>
+                    <Link className='w-full impLink'>মাধ্যমিক ও উচ্চশিক্ষা অধিদপ্তর</Link>
+                    <Link className='w-full impLink'>দিনাজপুর শিক্ষা বোর্ড</Link>
                 </div>
             </div>
             <div className='mb-4'>
                 <h6 className='w-full bg-primary text-white p-3'>সামাজিক যোগাযোগ</h6>
                 <div className="row">
-                    <Link className='w-full '>ফেইসবুক</Link>
-                    <Link className='w-full '> টুইটার</Link>
-                    <Link className='w-full '>ইউটিউব</Link>
+                    <Link className='w-full impLink'>ফেইসবুক</Link>
+                    <Link className='w-full impLink'> টুইটার</Link>
+                    <Link className='w-full impLink'>ইউটিউব</Link>
                 </div>
 
             </div>
